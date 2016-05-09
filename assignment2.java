a1356624410e96c6e1c1d792eee18ff5e3e07cc6
MishraN <mishran@advisory.com>
2016-05-09 11:31:35 +0530

    number format exception

---
+++ assignment2.java
@@ -0,0 +1,12 @@
+class assignment2
+{
+   public static void main(String args[])
+   {
+      try{
+	 int num=Integer.parseInt ("XYZ") ;
+	 System.out.println(num);
+      }catch(NumberFormatException e){
+	  System.out.println("Number format exception occurred");
+       }
+   }
+}
